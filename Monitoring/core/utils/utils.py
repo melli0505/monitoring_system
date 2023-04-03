4f96e98d8ebad203ce0ec13e574ef4a3524d4a27
melli0505 <dmelli0505@gmail.com>
2023-04-03 14:48:57 +0900

    implement authentication

---
+++ Monitoring/core/utils/utils.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timedelta
+from typing import Union, Any
+from jose import jwt
+from passlib.context import CryptContext
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import ValidationError
+from sqlalchemy.orm import Session
+
+from db import models, schemas, base
+from user import user_schema, user_crud
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+ALGORITHM = "HS256"
+SECRET_KEY = "9cd5be8a16f01bc9706ed047009068315e72c3df4fa86eee1b93581d3f5456ca"
+
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+reuseable_oauth = OAuth2PasswordBearer(
+    tokenUrl="/login",
+    scheme_name="JWT"
+)
+
+def get_db():
+    db = base.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+async def get_current_user(token: str = Depends(reuseable_oauth), db: Session = Depends(get_db)) -> models.User:
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        token_data = user_schema.TokenPayload(**payload)
+
+        if datetime.fromtimestamp(token_data.exp) < datetime.now():
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token Expired")
+    except(jwt.JWTError, ValidationError):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials")
+    
+    user = user_crud.get_user_by_username(db=db, username=token_data.sub)
+
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Could not find user")
+    
+    return user
+
+def get_hashed_password(password: str) -> str:
+    return password_context.hash(password)
+
+def verify_password(password:str, hashed_pass:str) -> bool:
+    return password_context.verify(password, hashed_pass)
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, 'sub': str(subject)}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+    return encoded_jwt
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.utcnow() + expires_delta
+    else:
+        expires_delta = datetime.utcnow() + timedelta(minutes=REFRESH_TOKEN_EXPIRE_MINUTES)
+
+    to_encode = {"exp": expires_delta, 'sub': str(subject)}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+    return encoded_jwt
