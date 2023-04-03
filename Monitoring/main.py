c6bf24cf31849716bb29452474a06e3c15054001
melli0505 <dmelli0505@gmail.com>
2023-04-03 14:46:40 +0900

    add login session & separated user handling part

--- Monitoring/main.py
+++ Monitoring/main.py
@@ -1,21 +1,20 @@
 from typing import List, Union
 from typing_extensions import Annotated
 
-from fastapi import FastAPI, Depends, HTTPException
+from jose import JWTError, jwt
+
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.responses import FileResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from sqlalchemy.orm import Session
 
-from core import crud
 from core.db import models, schemas
-from core.base import SessionLocal, engine
+from core.db.base import SessionLocal, engine
 
-models.Base.metadata.create_all(bind=engine)
+from core.user import user_router, user_crud
+from core.utils import utils, crud
 
-app = FastAPI()
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_db():
     db = SessionLocal()
@@ -24,23 +23,13 @@ def get_db():
     finally:
         db.close()
 
-def decode_token(token:str, db: Session = Depends(get_db)):
-    user = crud.get_user_by_username(db=db, username=token)
-    return user
+models.Base.metadata.create_all(bind=engine)
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)):
-    user = decode_token(db, token)
-    if not user:
-        raise HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    return user
+app = FastAPI()
 
-async def get_current_active_user(current_user: Annotated[models.User, Depends(get_current_user)]):
-    if current_user.disable:
-        raise HTTPException(status_code=400, detail="Inactive user")
+app.include_router(user_router.router)
+
+
 
 @app.get('/')
 def index():
@@ -51,42 +40,25 @@ def root():
     return {"message": "This is backend side API section."}
 
 # login
-@app.post("/token")
+@app.post("/login")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
-    user = crud.get_user_by_username(db=db, username=form_data.username)
-    if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    hashed_password = crud.create_hashpassword(form_data.password)
-    if not user.hashed_password == hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = user_crud.get_user_by_username(db=db, username=form_data.username)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
+    hashed_pass = user.hashed_password
+    if not utils.verify_password(form_data.password, hashed_pass):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password")
+    
+    return {
+        "access_token": utils.create_access_token(user.username),
+        "refresh_token": utils.create_refresh_token(user.username)
+    }
 
-    return {"access_token": user.username, "token_type": "bearer"}
-
-# users
-@app.post("/api/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_username(db=db, username=user.username)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered.")
-    return crud.create_user(db=db, user=user)
-
-@app.get("/api/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db=db, skip=skip, limit=limit)
-    return users
-
-@app.get("/api/users/{user_id}", response_model=schemas.User)
-def read_user(user_id:int, db: Session = Depends(get_db)):
-    user = crud.get_user(db=db, user_id=user_id)
-    if user == None:
-        raise HTTPException(status_code=404, detail="User not found")
+# user info
+@app.get('/me')
+async def get_me(user: models.User = Depends(utils.get_current_user)):
     return user
 
-@app.get("/users/me")
-async def read_users_me(
-    current_user: Annotated[models.User, Depends(get_current_active_user)]
-):
-    return current_user
 # data
 @app.get("/api/data/", response_model=List[schemas.Degree])
 def read_degrees(skip: int = 0, limit: int = 1000, db: Session = Depends(get_db)):
@@ -94,8 +66,8 @@ def read_degrees(skip: int = 0, limit: int = 1000, db: Session = Depends(get_db)
     return degrees
 
 @app.get("/api/data/{sensor_num}", response_model=List[schemas.Degree])
-def read_degrees_by_sensor_num(sensor_num: int = 0, db: Session = Depends(get_db)):
-    degrees = crud.get_degree_by_sensor_num(db=db, sensor_num=sensor_num)
+def read_degrees_by_sensor_num(id: int = 0, db: Session = Depends(get_db)):
+    degrees = crud.get_degree_by_sensor_num(db=db, sensor_id=id)
     return degrees
 
 @app.get("/api/data/{start}/{end}", response_model=List[schemas.Degree])
@@ -105,11 +77,14 @@ def read_degrees_by_date(start: str, end: str, db: Session = Depends(get_db)):
 
 # sensor
 @app.post("/api/sensor/", response_model=schemas.Sensor)
-def create_sensor(sensor: schemas.SensorCreate, db: Session = Depends(get_db)):
+def create_sensor(sensor: schemas.SensorCreate, username: str, db: Session = Depends(get_db)):
     exist_sensor = crud.get_sensor(db=db, id=sensor.id)
     if exist_sensor:
         raise HTTPException(status_code=400, detail="Sensor id already exist")
-    return crud.create_sensor(db=db, sensor=sensor)
+    user = user_crud.get_user_by_username(db=db, username=username)
+    if not user:
+        raise HTTPException(status_code=404, detail="Could not found user")
+    return crud.create_sensor(db=db, sensor=sensor, user=user)
 
 @app.get("/api/sensor/", response_model=List[schemas.Sensor])
 def read_sensors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
@@ -118,5 +93,5 @@ def read_sensors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db))
 
 @app.get("/api/sensor/{sensor_id}", response_model=schemas.Sensor)
 def read_sensor(sensor_id: int, db: Session = Depends(get_db)):
-    sensor = crud.get_sensor(db=db, sensor_id=sensor_id)
+    sensor = crud.get_sensor(db=db, id=sensor_id)
     return sensor
