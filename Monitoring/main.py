50241947d0096a1de359f343e52222d1887170dc
melli0505 <dmelli0505@gmail.com>
2023-04-06 09:20:11 +0900

    add middleware and connect to front

--- Monitoring/main.py
+++ Monitoring/main.py
@@ -1,17 +1,16 @@
 from typing import List, Union
 from typing_extensions import Annotated
 
-from jose import JWTError, jwt
-
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.responses import FileResponse
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import FastAPI, Depends, HTTPException, status, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
 from sqlalchemy.orm import Session
 
 from core.db import models, schemas
 from core.db.base import SessionLocal, engine
-
 from core.user import user_router, user_crud
 from core.utils import utils, crud
 
@@ -26,18 +25,42 @@ def get_db():
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 app.include_router(user_router.router)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get('/')
-def index():
-    return FileResponse("frontend/main.html")
+@app.get('/')#, response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get('/api/')
-def root():
-    return {"message": "This is backend side API section."}
+@app.get('/signup')
+def signup(request: Request):
+    return templates.TemplateResponse("signup.html", {"request": request})
+
+@app.get('/mypage')
+def my_page(request: Request):
+    return templates.TemplateResponse("me.html", {"request": request})
+
+@app.get('/dashboard')
+def show_dashboard(request:Request):
+    return templates.TemplateResponse("chart.html", {"request": request})
 
 # login
 @app.post("/login")
@@ -58,16 +81,21 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: 
 @app.get('/me')
 async def get_me(user: models.User = Depends(utils.get_current_user)):
     return user
+    
+# api section
+@app.get('/api/')
+def root():
+    return {"message": "This is backend side API section."}
 
 # data
 @app.get("/api/data/", response_model=List[schemas.Degree])
-def read_degrees(skip: int = 0, limit: int = 1000, db: Session = Depends(get_db)):
-    degrees = crud.get_degree(db=db, skip=skip, limit=limit)
+def read_degrees(db: Session = Depends(get_db)):
+    degrees = crud.get_degree(db=db)
     return degrees
 
 @app.get("/api/data/{sensor_num}", response_model=List[schemas.Degree])
-def read_degrees_by_sensor_num(id: int = 0, db: Session = Depends(get_db)):
-    degrees = crud.get_degree_by_sensor_num(db=db, sensor_id=id)
+def read_degrees_by_sensor_num(sensor_num: int, db: Session = Depends(get_db)):
+    degrees = crud.get_degree_by_sensor_num(db=db, sensor_id=sensor_num)
     return degrees
 
 @app.get("/api/data/{start}/{end}", response_model=List[schemas.Degree])
@@ -90,6 +118,11 @@ def create_sensor(sensor: schemas.SensorCreate, username: str, db: Session = Dep
 def read_sensors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     sensors = crud.get_sensors(db=db, skip=skip, limit=limit)
     return sensors
+
+@app.get("/api/sensor/sensor_info")
+def read_sensor_info(db: Session = Depends(get_db)):
+    sensor = crud.get_sensor_info(db=db)
+    return sensor
 
 @app.get("/api/sensor/{sensor_id}", response_model=schemas.Sensor)
 def read_sensor(sensor_id: int, db: Session = Depends(get_db)):
