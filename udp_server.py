6ea3cfe0e187c784db08df2b088b64b93955a7c7
melli0505 <dmelli0505@gmail.com>
2023-04-17 11:02:39 +0900

    add animation pause/restart action

--- udp_server.py
+++ udp_server.py
@@ -1,28 +1,40 @@
 import socket
-import struct
 import array
-import time
 from threading import Lock
 from _thread import *
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from matplotlib.widgets import Button
 
-def animate(i):
-    data_lock.acquire()
-    if len(entire_data) > 100000:
-        graph_data = entire_data[:50000]
-    else: 
-        graph_data = entire_data
-    data_lock.release()
-    ax.clear()
-    ax.plot(graph_data)
+class OnOffAnimation:
+    def __init__(self) -> None:
+        self.fig = plt.figure(figsize=(16, 4))
+        self.ax = self.fig.add_subplot(1, 1, 1)
+        self.ax.set_title("진동 센서")
+        
+        self.animation = animation.FuncAnimation(self.fig, self.update, interval=100)
+        self.paused = False
+        self.fig.canvas.mpl_connect("button_press_event", self.toggle_event)
+
+    def update(self, i) -> None:
+        data_lock.acquire()
+        if len(entire_data) > 100000:
+            graph_data = entire_data[30000:]
+        else: 
+            graph_data = entire_data
+        data_lock.release()
+        self.ax.clear()
+        self.ax.plot(graph_data)
+    
+    def toggle_event(self, *args, **kwargs):
+        if self.paused:
+            self.animation.event_source.start()
+        else:
+            self.animation.event_source.stop()
+        self.paused = not self.paused
 
     
 def run():
-    global serverSocket
-    global data_lock
-    global entire_data
-
     print("Connected", flush=True)
     while True:
         data = serverSocket.recv(8192)
@@ -34,21 +46,15 @@ def run():
         data_lock.release()
 
 if __name__ == "__main__":
-    server = ('10.0.0.119', 2001)
+    server = ('YOUR_IP_ADDRESS', 2001)
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSocket.bind(server) 
 
-
-    entire_data = []
+    entire_data = [0]
     data_lock = Lock()
-
-    fig = plt.figure(figsize=(10, 2))
-    ax = fig.add_subplot(1, 1, 1)
-
     
     start_new_thread(run, ())
 
-    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    animation = OnOffAnimation()
     plt.show() 
-    
     
