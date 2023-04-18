e29fe16f3d92c8a68323377ac5a75a7f6a5d085c
melli0505 <dmelli0505@gmail.com>
2023-04-18 11:55:39 +0900

    implemented real time fft graph

--- udp_server.py
+++ udp_server.py
@@ -1,30 +1,57 @@
-import socket
-import array
-from threading import Lock
-from _thread import *
+import socket, array, time
+from threading import Lock, Thread, Event
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.widgets import Button
 
-class OnOffAnimation:
+from scipy import signal
+import numpy as np
+from numpy.fft import fft, ifft, rfft
+
+class OriginalAnimation:
     def __init__(self) -> None:
-        self.fig = plt.figure(figsize=(16, 4))
-        self.ax = self.fig.add_subplot(1, 1, 1)
-        self.ax.set_title("진동 센서")
-        
+        self.fig = plt.figure(figsize=(20, 4))
+        self.original = self.fig.add_subplot(3, 1, 1)
+        self.fft_graph = self.fig.add_subplot(3, 1, 2)
+        self.stft_graph = self.fig.add_subplot(3, 1, 3)
+
         self.animation = animation.FuncAnimation(self.fig, self.update, interval=100)
+
         self.paused = False
-        self.fig.canvas.mpl_connect("button_press_event", self.toggle_event)
+        ax = plt.axes([0.8, 0.025, 0.1, 0.04])
+        self.button = Button(ax, "Stop")
+        self.button.on_clicked(self.toggle_event)
 
     def update(self, i) -> None:
+        # update original graph
         data_lock.acquire()
         if len(entire_data) > 100000:
-            graph_data = entire_data[30000:]
+            graph_data = entire_data[len(entire_data) - len(entire_data) // 3:]
         else: 
             graph_data = entire_data
         data_lock.release()
-        self.ax.clear()
-        self.ax.plot(graph_data)
+
+        self.original.clear()
+        self.original.plot(graph_data)
+
+        # update fft graph
+        sampling_rate = 20480
+
+        fft_data = fft(graph_data) / len(graph_data)
+        fft_data = fft_data[range(int(len(graph_data) / 2))]
+        N = len(fft_data)
+        n = np.arange(N)
+        T = N / sampling_rate
+        frequency = n / T
+        frequency = frequency[range(int(len(graph_data) / 2))]
+
+        self.fft_graph.clear()
+        self.fft_graph.plot(frequency, fft_data)
+
+
+    def window(self, index, max_index):
+        return 0.5 - 0.5 * np.cos(2 * np.pi * index / max_index)
     
     def toggle_event(self, *args, **kwargs):
         if self.paused:
@@ -33,30 +60,31 @@ class OnOffAnimation:
             self.animation.event_source.stop()
         self.paused = not self.paused
 
-    
 def run():
     print("Connected", flush=True)
-    while True:
-        data = serverSocket.recv(8192)
+    while not exit_signal.is_set():
+        data = serverSocket.recv(16384)
         signals = array.array('f')
         signals.frombytes(data)
-
         data_lock.acquire()
         entire_data.extend(signals)
         data_lock.release()
+    exit()
+
 
 if __name__ == "__main__":
-    server = ('YOUR_IP_ADDRESS', 2001)
     server = ('YOUR_IP_ADDRESS', 2001)
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSocket.bind(server) 
 
     entire_data = [0]
     data_lock = Lock()
-    
-    start_new_thread(run, ())
 
-    animation = OnOffAnimation()
+    exit_signal = Event()
+    t = Thread(target=run, args=())
+    t.start()
+    
+    animation = OriginalAnimation()
     plt.show() 
-    
 
+        
