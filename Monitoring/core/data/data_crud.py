bc28ac1227e81186e306f7fe0cba46ba68942515
melli0505 <dmelli0505@gmail.com>
2023-07-25 16:47:34 +0900

    update data table based on PZEM-004t data

--- Monitoring/core/data/data_crud.py
+++ Monitoring/core/data/data_crud.py
@@ -3,14 +3,61 @@ from sqlalchemy.orm import Session
 from core.db import models, schemas
 from core.utils import utils
 
-def get_degree(db:Session):
+def get_voltage(db:Session):
     # index 단위로 sensor data 추출
-    return db.query(models.Degree).all()
+    return db.query(models.Voltage).all()
 
-def get_degree_by_sensor_num(db: Session, sensor_id: int):
-    # sensor number에 따라 data 추출
-    return db.query(models.Degree).filter(models.Degree.sensor_id == sensor_id).all()
-
-def get_degree_by_date(db: Session, start: str, end: str):
+def get_voltage_by_date(db: Session, start: str, end: str):
     # 기간에 따라 data 추출
-    return db.query(models.Degree).filter(models.Degree.time > start, models.Degree.time < end).all()
+    return db.query(models.Voltage).filter(models.Voltage.time > start, models.Voltage.time < end).all()
+
+
+def get_current(db:Session):
+    # index 단위로 sensor data 추출
+    return db.query(models.Current).all()
+
+def get_current_by_date(db: Session, start: str, end: str):
+    # 기간에 따라 data 추출
+    return db.query(models.Current).filter(models.Current.time > start, models.Current.time < end).all()
+
+
+
+def get_power(db:Session):
+    # index 단위로 sensor data 추출
+    return db.query(models.Power).all()
+
+def get_power_by_date(db: Session, start: str, end: str):
+    # 기간에 따라 data 추출
+    return db.query(models.Power).filter(models.Power.time > start, models.Power.time < end).all()
+
+
+
+
+def get_energy(db:Session):
+    # index 단위로 sensor data 추출
+    return db.query(models.Energy).all()
+
+def get_energy_by_date(db: Session, start: str, end: str):
+    # 기간에 따라 data 추출
+    return db.query(models.Energy).filter(models.Energy.time > start, models.Energy.time < end).all()
+
+
+
+def get_pf(db:Session):
+    # index 단위로 sensor data 추출
+    return db.query(models.PF).all()
+
+def get_pf_by_date(db: Session, start: str, end: str):
+    # 기간에 따라 data 추출
+    return db.query(models.PF).filter(models.PF.time > start, models.PF.time < end).all()
+
+
+def get_frequency(db:Session):
+    # index 단위로 sensor data 추출
+    return db.query(models.Frequency).all()
+
+def get_frequency_by_date(db: Session, start: str, end: str):
+    # 기간에 따라 data 추출
+    return db.query(models.Frequency).filter(models.Frequency.time > start, models.Frequency.time < end).all()
+
+
--- Monitoring/core/data/data_router.py
+++ Monitoring/core/data/data_router.py
@@ -8,6 +8,8 @@ from core.db.base import SessionLocal
 
 from .data_crud import *
 
+from pzem_pub import publish_control
+
 def get_db():
     db = SessionLocal()
     try:
@@ -20,17 +22,57 @@ router = APIRouter(
     tags=['data']
 )
 
-@router.get("/", response_model=List[schemas.Degree])
-def read_degrees(db: Session = Depends(get_db)):
-    degrees = get_degree(db=db)
-    return degrees
+@router.get("/voltage", response_model=List[schemas.Voltage])
+def read_voltage(db: Session = Depends(get_db)):
+    return get_voltage(db=db)
 
-@router.get("/{sensor_num}", response_model=List[schemas.Degree])
-def read_degrees_by_sensor_num(sensor_num: int, db: Session = Depends(get_db)):
-    degrees = get_degree_by_sensor_num(db=db, sensor_id=sensor_num)
-    return degrees
+@router.get("/energy", response_model=List[schemas.Energy])
+def read_energy(db: Session = Depends(get_db)):\
+    return get_energy(db=db)
 
-@router.get("/{start}/{end}", response_model=List[schemas.Degree])
+@router.get("/current", response_model=List[schemas.Current])
+def read_current(db: Session = Depends(get_db)):
+    return get_current(db=db)
+
+@router.get("/power", response_model=List[schemas.Power])
+def read_power(db: Session = Depends(get_db)):
+    return get_power(db=db)
+
+@router.get("/pf", response_model=List[schemas.PF])
+def read_pf(db: Session = Depends(get_db)):
+    return get_pf(db=db)
+
+@router.get("/frequency", response_model=List[schemas.Frequency])
+def read_frequency(db: Session = Depends(get_db)):
+    return get_frequency(db=db)
+
+
+@router.get("/voltage/{start}/{end}", response_model=List[schemas.Voltage])
 def read_degrees_by_date(start: str, end: str, db: Session = Depends(get_db)):
-    degrees = get_degree_by_date(db=db, start=start, end=end)
-    return degrees
+    return get_voltage_by_date(db=db, start=start, end=end)
+
+@router.get("/energy/{start}/{end}", response_model=List[schemas.Energy])
+def read_energy_by_date(start: str, end: str, db: Session = Depends(get_db)):
+    return get_energy_by_date(db=db, start=start, end=end)
+
+@router.get("/current/{start}/{end}", response_model=List[schemas.Current])
+def read_current_by_date(start: str, end: str, db: Session = Depends(get_db)):
+    return get_current_by_date(db=db, start=start, end=end)
+
+@router.get("/power/{start}/{end}", response_model=List[schemas.Power])
+def read_power_by_date(start: str, end: str, db: Session = Depends(get_db)):
+    return get_power_by_date(db=db, start=start, end=end)
+
+@router.get("/pf/{start}/{end}", response_model=List[schemas.PF])
+def read_pf_by_date(start: str, end: str, db: Session = Depends(get_db)):
+    return get_pf_by_date(db=db, start=start, end=end)
+
+@router.get("/frequency/{start}/{end}", response_model=List[schemas.Frequency])
+def read_frequency_by_date(start: str, end: str, db: Session = Depends(get_db)):
+    return get_frequency_by_date(db=db, start=start, end=end)
+
+
+@router.get("/reset_energy_counter")
+def reset_energy_counter():
+    publish_control("reset")
+    return True
